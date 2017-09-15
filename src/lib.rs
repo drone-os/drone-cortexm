1f49c2cc1792434bfedad480ba153a89fdaccbcb
Valentine Valyaeff <valentine.valyaeff@gmail.com>
2017-09-15 20:10:49 +0300

    Using routine API

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,6 @@
 //! *Drone* bindings for *STM32* microcontrollers.
-#![feature(alloc)]
 #![feature(asm)]
 #![feature(const_fn)]
-#![feature(drop_types_in_const)]
-#![feature(fnbox)]
-#![feature(generators)]
-#![feature(generator_trait)]
 #![feature(lang_items)]
 #![feature(linkage)]
 #![no_std]
@@ -15,7 +10,6 @@
 #![cfg_attr(feature = "clippy", plugin(clippy))]
 #![cfg_attr(feature = "clippy", allow(precedence, doc_markdown))]
 
-extern crate alloc;
 #[cfg(test)]
 extern crate compiler_builtins;
 #[macro_use]
--- src/vtable.rs
+++ src/vtable.rs
@@ -1,25 +1,10 @@
 //! The vector table of interrupt service routines.
 
-use alloc::boxed::FnBox;
-use core::mem;
-use core::ops::{Generator, GeneratorState};
-use drone::prelude::*;
-
 /// Pointer to an exception routine.
 pub type Handler = unsafe extern "C" fn();
 
 /// Pointer to a reset routine.
 pub type ResetHandler = unsafe extern "C" fn() -> !;
-
-/// Data attached to a routine.
-pub enum RoutineData {
-  /// No associated data.
-  Empty,
-  /// Attached generator.
-  Generator(Box<Generator<Yield = (), Return = ()>>),
-  /// Attached closure.
-  Callback(Box<FnBox()>),
-}
 
 /// Reserved pointer in a vector table.
 #[derive(Clone, Copy)]
@@ -27,43 +12,6 @@ pub enum RoutineData {
 pub enum Reserved {
   /// The only allowed zero-value.
   Vector = 0,
-}
-
-#[doc(hidden)]
-pub fn routine_handler(data: &mut RoutineData) {
-  match mem::replace(data, RoutineData::Empty) {
-    RoutineData::Generator(mut generator) => match generator.resume() {
-      GeneratorState::Yielded(()) => {
-        *data = RoutineData::Generator(generator);
-      }
-      GeneratorState::Complete(()) => {}
-    },
-    RoutineData::Callback(callback) => {
-      callback();
-    }
-    RoutineData::Empty => {}
-  }
-}
-
-#[doc(hidden)]
-pub fn routine_append<G>(data: &mut RoutineData, g: G)
-where
-  G: Generator<Yield = (), Return = ()> + Send + 'static,
-{
-  *data = RoutineData::Generator(Box::new(g));
-}
-
-#[doc(hidden)]
-pub fn routine_append_callback<F>(data: &mut RoutineData, f: F)
-where
-  F: FnOnce() + Send + 'static,
-{
-  *data = RoutineData::Callback(Box::new(f));
-}
-
-#[doc(hidden)]
-pub fn routine_clear(data: &mut RoutineData) {
-  *data = RoutineData::Empty;
 }
 
 #[doc(hidden)]
@@ -166,111 +114,24 @@ macro_rules! vtable {
     $(
       $(#[$meta])*
       pub mod $vector {
-        static mut DATA: $crate::vtable::RoutineData =
-          $crate::vtable::RoutineData::Empty;
+        /// The beginning of the routine chain.
+        pub static mut ROUTINE: ::drone::routine::RoutineList =
+          ::drone::routine::RoutineList::vacant();
 
         /// The routine handler.
         ///
         /// # Safety
         ///
-        /// Must be called only by hardware.
+        /// Should be called only by hardware.
         pub unsafe extern "C" fn handler() {
-          $crate::vtable::routine_handler(&mut DATA);
+          ROUTINE.invoke();
         }
+      }
 
-        /// Appends `Generator` routine handler.
-        ///
-        /// Generator `g` will be resumed each time the handler is called. Will
-        /// be removed once got complete.
-        pub fn append<G>(g: G)
-        where
-          G: ::core::ops::Generator<Yield = (), Return = ()> + Send + 'static,
-        {
-          $crate::vtable::routine_append(unsafe { &mut DATA }, g);
-        }
-
-        /// Appends `FnOnce` routine handler.
-        ///
-        /// Will be invoked once when the handler is called, and then removed.
-        pub fn append_callback<F>(f: F)
-        where
-          F: FnOnce() + Send + 'static,
-        {
-          $crate::vtable::routine_append_callback(unsafe { &mut DATA }, f);
-        }
-
-        /// Clears all attached handlers.
-        pub fn clear() {
-          $crate::vtable::routine_clear(unsafe { &mut DATA });
-        }
+      $(#[$meta])*
+      pub fn $vector() -> &'static mut ::drone::routine::RoutineList {
+        unsafe { &mut $vector::ROUTINE }
       }
     )*
   };
-}
-
-#[cfg(test)]
-pub mod tests {
-  use alloc::arc::Arc;
-  use drone::ALLOCATOR;
-  use drone::sync::Mutex;
-
-  vtable!(nmi,);
-
-  #[allow(dead_code)]
-  static VECTOR_TABLE: VectorTable = VectorTable::new(main);
-
-  extern "C" fn main() -> ! {
-    loop {}
-  }
-
-  #[test]
-  fn generator() {
-    let heap = [0; 1024];
-    unsafe {
-      ALLOCATOR.lock().init(&heap as *const i32 as usize, 1024);
-    }
-    let x = Arc::new(Mutex::new(0));
-    let y = Arc::clone(&x);
-    nmi::append(move || loop {
-      {
-        let mut counter = x.lock();
-        *counter += 1;
-      }
-      yield;
-    });
-    assert_eq!(*y.lock(), 0);
-    unsafe {
-      nmi::handler();
-    }
-    assert_eq!(*y.lock(), 1);
-    unsafe {
-      nmi::handler();
-    }
-    assert_eq!(*y.lock(), 2);
-    nmi::clear();
-  }
-
-  #[test]
-  fn callback() {
-    let heap = [0; 1024];
-    unsafe {
-      ALLOCATOR.lock().init(&heap as *const i32 as usize, 1024);
-    }
-    let x = Arc::new(Mutex::new(0));
-    let y = Arc::clone(&x);
-    nmi::append_callback(move || {
-      let mut counter = x.lock();
-      *counter += 1;
-    });
-    assert_eq!(*y.lock(), 0);
-    unsafe {
-      nmi::handler();
-    }
-    assert_eq!(*y.lock(), 1);
-    unsafe {
-      nmi::handler();
-    }
-    assert_eq!(*y.lock(), 1);
-    nmi::clear();
-  }
 }
