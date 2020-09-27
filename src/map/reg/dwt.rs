7f76152b752f29403fc5913687c4fd567877cc28
Valentine Valyaeff <valentine.valyaeff@gmail.com>
2020-09-27 11:57:45 +0300

    Fully map DWT_CTRL register

--- src/map/reg/dwt.rs
+++ src/map/reg/dwt.rs
@@ -2,11 +2,48 @@ use crate::reg::prelude::*;
 use drone_core::reg;
 
 reg! {
-    /// DWT Control Register to enable the DWT unit.
-    pub mod DWT DWTCTRL;
+    /// Control Register.
+    pub mod DWT CTRL;
     0xE000_1000 0x20 0x0000_0000
     RReg WReg;
-    /// Enable the CYCCNT counter. 
+    /// Number of comparators implemented.
+    NUMCOMP { 28 4 RRRegField }
+    /// Shows whether the implementation supports trace sampling and execution
+    /// tracing.
+    NOTRCPKT { 27 1 RRRegField }
+    /// Shows whether the implementation includes external match signals.
+    NOEXTTRIG { 26 1 RRRegField }
+    /// Shows whether the implementation supports a cycle counter.
+    NOCYCCNT { 25 1 RRRegField }
+    /// Shows whether the implementation supports the profiling counters.
+    NOPRFCNT { 24 1 RRRegField }
+    /// Enables POSTCNT underflow Event counter packets generation.
+    CYCEVTENA { 22 1 RRRegField WWRegField }
+    /// Enables generation of the Folded-instruction counter overflow event.
+    FOLDEVTENA { 21 1 RRRegField WWRegField }
+    /// Enables generation of the LSU counter overflow event.
+    LSUEVTENA { 20 1 RRRegField WWRegField }
+    /// Enables generation of the Sleep counter overflow event.
+    SLEEPEVTENA { 19 1 RRRegField WWRegField }
+    /// Enables generation of the Exception overhead counter overflow event.
+    EXCEVTENA { 18 1 RRRegField WWRegField }
+    /// Enables generation of the CPI counter overflow event.
+    CPIEVTENA { 17 1 RRRegField WWRegField }
+    /// Enables generation of exception trace.
+    EXCTRCENA { 16 1 RRRegField WWRegField }
+    /// Enables use of POSTCNT counter as a timer for Periodic PC sample packet
+    /// generation.
+    PCSAMPLEENA { 12 1 RRRegField WWRegField }
+    /// Selects the position of the synchronization packet counter tap on the
+    /// CYCCNT counter.
+    SYNCTAP { 10 2 RRRegField WWRegField }
+    /// Selects the position of the POSTCNT tap on the CYCCNT counter.
+    CYCTAP { 9 1 RRRegField WWRegField }
+    /// Initial value for the POSTCNT counter.
+    POSTINIT { 5 4 RRRegField WWRegField }
+    /// Reload value for the POSTCNT counter.
+    POSTPRESET { 1 4 RRRegField WWRegField }
+    /// Enables CYCCNT.
     CYCCNTENA { 0 1 RRRegField WWRegField }
 }
 
