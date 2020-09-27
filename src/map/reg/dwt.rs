7e401688000153aa3487363e76aae189c63d0f51
Valentine Valyaeff <valentine.valyaeff@gmail.com>
2020-09-27 11:56:47 +0300

    Remove redundant DEMCR register mapping

--- src/map/reg/dwt.rs
+++ src/map/reg/dwt.rs
@@ -18,12 +18,3 @@ reg! {
     /// Incrementing cycle counter value.
     CYCCNT { 0 32 RRRegField WWRegField }
 }
-
-reg! {
-    /// Control Register
-    pub mod DWT DEMCR;
-    0xE000_EDFC 0x20 0x0000_0000
-    RReg WReg;
-    /// This enables control of power usage unless tracing is required. 
-    TRCENA { 24 1 RRRegField WWRegField }
-}
