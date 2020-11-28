36d78f347248181b46b6069cf8aebd8b6e2b4551
Valentine Valyaeff <valentine.valyaeff@gmail.com>
2020-11-28 19:52:58 +0200

    Fix DWT_CTRL register mapping

--- src/map/reg/dwt.rs
+++ src/map/reg/dwt.rs
@@ -3,48 +3,57 @@ use drone_core::reg;
 
 reg! {
     /// Control Register.
-    pub mod DWT CTRL;
-    0xE000_1000 0x20 0x0000_0000
-    RReg WReg;
-    /// Number of comparators implemented.
-    NUMCOMP { 28 4 RRRegField }
-    /// Shows whether the implementation supports trace sampling and execution
-    /// tracing.
-    NOTRCPKT { 27 1 RRRegField }
-    /// Shows whether the implementation includes external match signals.
-    NOEXTTRIG { 26 1 RRRegField }
-    /// Shows whether the implementation supports a cycle counter.
-    NOCYCCNT { 25 1 RRRegField }
-    /// Shows whether the implementation supports the profiling counters.
-    NOPRFCNT { 24 1 RRRegField }
-    /// Enables POSTCNT underflow Event counter packets generation.
-    CYCEVTENA { 22 1 RRRegField WWRegField }
-    /// Enables generation of the Folded-instruction counter overflow event.
-    FOLDEVTENA { 21 1 RRRegField WWRegField }
-    /// Enables generation of the LSU counter overflow event.
-    LSUEVTENA { 20 1 RRRegField WWRegField }
-    /// Enables generation of the Sleep counter overflow event.
-    SLEEPEVTENA { 19 1 RRRegField WWRegField }
-    /// Enables generation of the Exception overhead counter overflow event.
-    EXCEVTENA { 18 1 RRRegField WWRegField }
-    /// Enables generation of the CPI counter overflow event.
-    CPIEVTENA { 17 1 RRRegField WWRegField }
-    /// Enables generation of exception trace.
-    EXCTRCENA { 16 1 RRRegField WWRegField }
-    /// Enables use of POSTCNT counter as a timer for Periodic PC sample packet
-    /// generation.
-    PCSAMPLEENA { 12 1 RRRegField WWRegField }
-    /// Selects the position of the synchronization packet counter tap on the
-    /// CYCCNT counter.
-    SYNCTAP { 10 2 RRRegField WWRegField }
-    /// Selects the position of the POSTCNT tap on the CYCCNT counter.
-    CYCTAP { 9 1 RRRegField WWRegField }
-    /// Initial value for the POSTCNT counter.
-    POSTINIT { 5 4 RRRegField WWRegField }
-    /// Reload value for the POSTCNT counter.
-    POSTPRESET { 1 4 RRRegField WWRegField }
-    /// Enables CYCCNT.
-    CYCCNTENA { 0 1 RRRegField WWRegField }
+    pub DWT CTRL => {
+        address => 0xE000_1000;
+        size => 0x20;
+        reset => 0x0000_0000;
+        traits => { RReg WReg };
+        fields => {
+            /// Number of comparators implemented.
+            NUMCOMP => { offset => 28; width => 4; traits => { RRRegField } };
+            /// Shows whether the implementation supports trace sampling and
+            /// execution tracing.
+            NOTRCPKT => { offset => 27; width => 1; traits => { RRRegField } };
+            /// Shows whether the implementation includes external match
+            /// signals.
+            NOEXTTRIG => { offset => 26; width => 1; traits => { RRRegField } };
+            /// Shows whether the implementation supports a cycle counter.
+            NOCYCCNT => { offset => 25; width => 1; traits => { RRRegField } };
+            /// Shows whether the implementation supports the profiling
+            /// counters.
+            NOPRFCNT => { offset => 24; width => 1; traits => { RRRegField } };
+            /// Enables POSTCNT underflow Event counter packets generation.
+            CYCEVTENA => { offset => 22; width => 1; traits => { RRRegField WWRegField } };
+            /// Enables generation of the Folded-instruction counter overflow
+            /// event.
+            FOLDEVTENA => { offset => 21; width => 1; traits => { RRRegField WWRegField } };
+            /// Enables generation of the LSU counter overflow event.
+            LSUEVTENA => { offset => 20; width => 1; traits => { RRRegField WWRegField } };
+            /// Enables generation of the Sleep counter overflow event.
+            SLEEPEVTENA => { offset => 19; width => 1; traits => { RRRegField WWRegField } };
+            /// Enables generation of the Exception overhead counter overflow
+            /// event.
+            EXCEVTENA => { offset => 18; width => 1; traits => { RRRegField WWRegField } };
+            /// Enables generation of the CPI counter overflow event.
+            CPIEVTENA => { offset => 17; width => 1; traits => { RRRegField WWRegField } };
+            /// Enables generation of exception trace.
+            EXCTRCENA => { offset => 16; width => 1; traits => { RRRegField WWRegField } };
+            /// Enables use of POSTCNT counter as a timer for Periodic PC sample
+            /// packet generation.
+            PCSAMPLEENA => { offset => 12; width => 1; traits => { RRRegField WWRegField } };
+            /// Selects the position of the synchronization packet counter tap
+            /// on the CYCCNT counter.
+            SYNCTAP => { offset => 10; width => 2; traits => { RRRegField WWRegField } };
+            /// Selects the position of the POSTCNT tap on the CYCCNT counter.
+            CYCTAP => { offset => 9; width => 1; traits => { RRRegField WWRegField } };
+            /// Initial value for the POSTCNT counter.
+            POSTINIT => { offset => 5; width => 4; traits => { RRRegField WWRegField } };
+            /// Reload value for the POSTCNT counter.
+            POSTPRESET => { offset => 1; width => 4; traits => { RRRegField WWRegField } };
+            /// Enables CYCCNT.
+            CYCCNTENA => { offset => 0; width => 1; traits => { RRRegField WWRegField } };
+        };
+    };
 }
 
 reg! {
